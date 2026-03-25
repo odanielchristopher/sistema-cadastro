67bbdbdea6657160d3c6b94bf00cd973d4f3e78c
daniel christopher <daniel.2005.christopher@gmail.com>
2026-03-24 21:48:12 -0300

    feat: add color picker

--- apps/fe/src/views/components/ui/Select.tsx
+++ apps/fe/src/views/components/ui/Select.tsx
@@ -1,103 +1,188 @@
-import { ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
-import { Select as RdxSelect } from 'radix-ui';
-import { useState } from 'react';
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
+import { Select as SelectPrimitive } from 'radix-ui';
+import * as React from 'react';
 
 import { cn } from '@app/lib/utils';
 
-import { FieldError } from './FieldError';
-
-interface ISelectProps {
-  error?: string;
-  className?: string;
-  placeholder?: string;
-  options: {
-    value: string;
-    label: string;
-  }[];
-  value?: string;
-  onChange?(value: string): void;
+function Select({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Root>) {
+  return <SelectPrimitive.Root data-slot="select" {...props} />;
 }
 
-export function Select({
-  error,
-  value,
-  options,
+function SelectGroup({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Group>) {
+  return <SelectPrimitive.Group data-slot="select-group" {...props} />;
+}
+
+function SelectValue({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Value>) {
+  return <SelectPrimitive.Value data-slot="select-value" {...props} />;
+}
+
+function SelectTrigger({
   className,
-  placeholder,
-  onChange,
-}: ISelectProps) {
-  const [selectedValue, setSelectedValue] = useState(value ?? '');
-
-  function handleSelect(newValue: string) {
-    setSelectedValue(newValue);
-    onChange?.(newValue);
-  }
-
+  size = 'default',
+  children,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Trigger> & {
+  size?: 'sm' | 'default';
+}) {
   return (
-    <div>
-      <div className="relative">
-        <label
-          className={cn(
-            'text-muted-foreground pointer-events-none absolute left-3 top-[26px] z-10 -translate-y-1/2 transition-all',
-            selectedValue && 'left-[13px] top-2 translate-y-0 text-xs',
-          )}
-        >
-          {placeholder}
-        </label>
-
-        <RdxSelect.Root value={selectedValue} onValueChange={handleSelect}>
-          <RdxSelect.Trigger
-            className={cn(
-              'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex min-w-0 rounded-md px-3 py-1 pt-5 text-sm outline-none transition-all file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-              'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-              'dark:border-accent dark:text-foreground h-[52px] w-full rounded-lg border border-gray-500 bg-white text-gray-700',
-              error && '!border-red-900',
-              className,
-            )}
-          >
-            <RdxSelect.Value />
-            <RdxSelect.Icon className="absolute right-3 top-1/2 -translate-y-1/2">
-              <ChevronDownIcon className="dark:text-popover-foreground h-6 w-6 text-gray-800" />
-            </RdxSelect.Icon>
-          </RdxSelect.Trigger>
-
-          <RdxSelect.Portal>
-            <RdxSelect.Content
-              className={cn(
-                'dark:bg-popover z-[99] overflow-hidden rounded-md border bg-white shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]',
-                'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
-              )}
-            >
-              <RdxSelect.ScrollUpButton className="flex h-[25px] cursor-default items-center justify-center bg-white text-gray-800">
-                <ChevronUpIcon />
-              </RdxSelect.ScrollUpButton>
-
-              <RdxSelect.Viewport className="p-2">
-                {options.map((option) => (
-                  <RdxSelect.Item
-                    key={option.value}
-                    value={option.value}
-                    data-slot="select-item"
-                    className={cn(
-                      'dark:text-popover-foreground rounded-lg p-2 text-sm text-gray-800 outline-none',
-                      'hover:bg-accent focus:bg-accent focus:text-accent-foreground cursor-pointer data-[state=checked]:font-bold',
-                      'dark:data-[highlighted]:bg-accent data-[highlighted]:bg-gray-50',
-                    )}
-                  >
-                    <RdxSelect.ItemText>{option.label}</RdxSelect.ItemText>
-                  </RdxSelect.Item>
-                ))}
-              </RdxSelect.Viewport>
-
-              <RdxSelect.ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-white text-gray-800">
-                <ChevronDownIcon />
-              </RdxSelect.ScrollDownButton>
-            </RdxSelect.Content>
-          </RdxSelect.Portal>
-        </RdxSelect.Root>
-      </div>
-
-      {error && <FieldError message={error} />}
-    </div>
+    <SelectPrimitive.Trigger
+      data-slot="select-trigger"
+      data-size={size}
+      className={cn(
+        "border-input shadow-xs focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[placeholder]:text-muted-foreground dark:bg-input/30 dark:hover:bg-input/50 dark:aria-invalid:ring-destructive/40 [&_svg:not([class*='text-'])]:text-muted-foreground flex w-fit items-center justify-between gap-2 whitespace-nowrap rounded-md border bg-transparent px-3 py-2 text-sm outline-none transition-[color,box-shadow] focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 data-[size=default]:h-9 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <SelectPrimitive.Icon asChild>
+        <ChevronDownIcon className="size-4 opacity-50" />
+      </SelectPrimitive.Icon>
+    </SelectPrimitive.Trigger>
   );
 }
+
+function SelectContent({
+  className,
+  children,
+  position = 'item-aligned',
+  align = 'center',
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Content>) {
+  return (
+    <SelectPrimitive.Portal>
+      <SelectPrimitive.Content
+        data-slot="select-content"
+        className={cn(
+          'max-h-(--radix-select-content-available-height) origin-(--radix-select-content-transform-origin) bg-popover text-popover-foreground data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 relative z-50 min-w-[8rem] overflow-y-auto overflow-x-hidden rounded-md border shadow-md',
+          position === 'popper' &&
+            'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
+          className,
+        )}
+        position={position}
+        align={align}
+        {...props}
+      >
+        <SelectScrollUpButton />
+        <SelectPrimitive.Viewport
+          className={cn(
+            'p-1',
+            position === 'popper' &&
+              'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1',
+          )}
+        >
+          {children}
+        </SelectPrimitive.Viewport>
+        <SelectScrollDownButton />
+      </SelectPrimitive.Content>
+    </SelectPrimitive.Portal>
+  );
+}
+
+function SelectLabel({
+  className,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Label>) {
+  return (
+    <SelectPrimitive.Label
+      data-slot="select-label"
+      className={cn('text-muted-foreground px-2 py-1.5 text-xs', className)}
+      {...props}
+    />
+  );
+}
+
+function SelectItem({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Item>) {
+  return (
+    <SelectPrimitive.Item
+      data-slot="select-item"
+      className={cn(
+        "outline-hidden focus:bg-accent focus:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2 relative flex w-full cursor-default select-none items-center gap-2 rounded-sm py-1.5 pl-2 pr-8 text-sm data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+        className,
+      )}
+      {...props}
+    >
+      <span
+        data-slot="select-item-indicator"
+        className="absolute right-2 flex size-3.5 items-center justify-center"
+      >
+        <SelectPrimitive.ItemIndicator>
+          <CheckIcon className="size-4" />
+        </SelectPrimitive.ItemIndicator>
+      </span>
+      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+    </SelectPrimitive.Item>
+  );
+}
+
+function SelectSeparator({
+  className,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Separator>) {
+  return (
+    <SelectPrimitive.Separator
+      data-slot="select-separator"
+      className={cn('bg-border pointer-events-none -mx-1 my-1 h-px', className)}
+      {...props}
+    />
+  );
+}
+
+function SelectScrollUpButton({
+  className,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.ScrollUpButton>) {
+  return (
+    <SelectPrimitive.ScrollUpButton
+      data-slot="select-scroll-up-button"
+      className={cn(
+        'flex cursor-default items-center justify-center py-1',
+        className,
+      )}
+      {...props}
+    >
+      <ChevronUpIcon className="size-4" />
+    </SelectPrimitive.ScrollUpButton>
+  );
+}
+
+function SelectScrollDownButton({
+  className,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.ScrollDownButton>) {
+  return (
+    <SelectPrimitive.ScrollDownButton
+      data-slot="select-scroll-down-button"
+      className={cn(
+        'flex cursor-default items-center justify-center py-1',
+        className,
+      )}
+      {...props}
+    >
+      <ChevronDownIcon className="size-4" />
+    </SelectPrimitive.ScrollDownButton>
+  );
+}
+
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+};
--- apps/fe/src/views/pages/Dashboard/index.tsx
+++ apps/fe/src/views/pages/Dashboard/index.tsx
@@ -1,10 +1,22 @@
-import { HomeIcon, LogOutIcon } from 'lucide-react';
+import {
+  HomeIcon,
+  LogOutIcon,
+  PaletteIcon,
+  PlusIcon,
+  Users2Icon,
+} from 'lucide-react';
 
 import { Button } from '@views/components/ui/Button';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@views/components/ui/Tabs';
 
 export function Dashboard() {
   return (
-    <>
+    <div className="flex h-full w-full flex-col">
       <header className="bg-card border-b py-4">
         <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-6">
           <div className="flex flex-col">
@@ -29,7 +41,63 @@ export function Dashboard() {
         </div>
       </header>
 
-      <main className=""></main>
-    </>
+      <main className="mx-auto mt-10 flex w-full max-w-7xl flex-1 px-6">
+        <Tabs
+          className="mx-auto flex w-full flex-1 flex-col items-center gap-6 pb-6"
+          defaultValue="clients"
+        >
+          <TabsList className="w-full max-w-[600px] rounded-2xl">
+            <TabsTrigger className="rounded-2xl" value="clients">
+              <Users2Icon />
+              Clientes
+            </TabsTrigger>
+            <TabsTrigger value="colors">
+              <PaletteIcon />
+              Cores
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent
+            className="bg-card h-full w-full rounded-2xl border p-6"
+            value="clients"
+          >
+            <header>
+              <div className="mb-2 flex items-center gap-3">
+                <Users2Icon />
+                <h3 className="text-lg font-semibold">Clientes cadastrados</h3>
+              </div>
+
+              <p className="text-muted-foreground">
+                Lista de todos os clientes que se cadastraram no sistema
+              </p>
+            </header>
+          </TabsContent>
+
+          <TabsContent
+            className="bg-card h-full w-full rounded-2xl border p-6"
+            value="colors"
+          >
+            <header className="flex items-center justify-between">
+              <div>
+                <div className="mb-2 flex items-center gap-3">
+                  <PaletteIcon />
+                  <h3 className="text-lg font-semibold">
+                    Gerenciamento de Cores
+                  </h3>
+                </div>
+
+                <p className="text-muted-foreground">
+                  Gerencie as cores disponíveis no arco-íris para cadastro
+                </p>
+              </div>
+
+              <Button type="button" className="rounded-sm" size="sm">
+                <PlusIcon /> Adicionar nova cor
+              </Button>
+            </header>
+          </TabsContent>
+        </Tabs>
+      </main>
+    </div>
   );
 }
